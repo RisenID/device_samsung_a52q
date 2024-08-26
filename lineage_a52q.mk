f3739144196d0a542ece8ff8853d9282f1e494a3
Ruchit <risenid@duck.com>
2024-08-26 21:48:25 +0530

    rising: riseup
    Change-Id: Ie0c143d635267b899914e5b34c5deeecbd0dcd1d
    Signed-off-by: Ruchit <risenid@duck.com>

--- lineage_a52q.mk
+++ lineage_a52q.mk
@@ -24,7 +24,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
@@ -35,7 +35,7 @@ TARGET_SCREEN_WIDTH := 1080
 TARGET_BOOT_ANIMATION_RES := 1080
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := aosp_a52q
+PRODUCT_NAME := lineage_a52q
 PRODUCT_DEVICE := a52q
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A525F
